0ef9dac534d99c58ce5b1f83af1abf34dc1b2571
Dhaval Sindhav <91953964+dhaval7536@users.noreply.github.com>
2022-10-11 01:22:40 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const noteRouter = require("./routes/notesRoutes");
+const userRouter = require("./routes/userRoutes");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cors = require("cors");
+dotenv.config();
+
+app.use(express.json());
+app.use(cors());
+app.use("/users", userRouter);
+app.use("/note", noteRouter);
+
+app.get("/", (req, res) => {
+  res.send("Notes API  From Dhaval Sindhav");
+});
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Server started on port no." + PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
